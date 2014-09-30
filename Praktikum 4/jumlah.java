bc134ebe685e4be7b08c03d6083659a034b4fb04
handrofina <handrofina@gmail.com>
2014-09-30 15:04:31 +0700

    pert4

---
+++ Praktikum 4/jumlah.java
@@ -0,0 +1,10 @@
+public class jumlah {
+public static void main (String[]args) {
+int i,jumlah;
+jumlah = 1;
+		for (i=0; i<=10; i++){
+		jumlah = jumlah +i;
+		System.out.println (jumlah);
+}
+}
+}
